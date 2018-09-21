db2c103e3b4b8b18ae92f0bd2b2df054e6da17f2
linuxmint <linuxmint@linuxmint>
2018-09-21 10:13:24 +0200

    added niew source file "plik_.c" with dome methods declared

---
+++ plik_.c
@@ -0,0 +1,20 @@
+#define UP 	1
+#define DN	0
+
+
+
+void funkcja(int masakra)
+{
+switch(masakra)
+    {
+    case 0:
+	break;
+    case 1:
+	break;
+    case 2:
+	break;
+    default:
+	break;
+    }
+
+}
